f6e7035d4ee6306655381d06c8cbf02739c0b3fa
Mike Dalessio <mike@37signals.com>
2025-02-17 12:25:42 -0500

    prefactor: move db config handler into DatabaseConfigurations

--- lib/active_record/tenanted/database_configurations.rb
+++ lib/active_record/tenanted/database_configurations.rb
@@ -79,6 +79,15 @@ module ActiveRecord
           end
         end
       end
+
+      # Invoked by the railtie
+      def self.register_db_config_handler # :nodoc:
+        ActiveRecord::DatabaseConfigurations.register_db_config_handler do |env_name, name, _, config|
+          next unless config.fetch(:tenanted, false)
+
+          ActiveRecord::Tenanted::DatabaseConfigurations::RootConfig.new(env_name, name, config)
+        end
+      end
     end
   end
 end
--- lib/active_record/tenanted/railtie.rb
+++ lib/active_record/tenanted/railtie.rb
@@ -26,10 +26,7 @@ module ActiveRecord
 
       config.before_configuration do
         ActiveSupport.on_load(:active_record) do
-          ActiveRecord::DatabaseConfigurations.register_db_config_handler do |env_name, name, _, config|
-            next unless config.fetch(:tenanted, false)
-            ActiveRecord::Tenanted::DatabaseConfigurations::RootConfig.new(env_name, name, config)
-          end
+          ActiveRecord::Tenanted::DatabaseConfigurations.register_db_config_handler
         end
       end
 
