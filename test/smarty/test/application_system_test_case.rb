f3744eea468777861c6c4e52424e4d319386a375
Mike Dalessio <mike@37signals.com>
2025-02-21 10:14:23 -0500

    test: suppress puma's boot announcement during system tests

--- test/smarty/test/application_system_test_case.rb
+++ test/smarty/test/application_system_test_case.rb
@@ -1,5 +1,7 @@
 require "test_helper"
 
+Capybara.server = :puma, { Silent: true } # suppress server boot announcement
+
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ]
 end
