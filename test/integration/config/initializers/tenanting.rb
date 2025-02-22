ffc53ce781268097708625eab6b1a57b0ab7784c
Mike Dalessio <mike@37signals.com>
2025-02-22 14:47:26 -0500

    dev: Config integration test to use tenant subdomain in dev
    so I can run the server when I need to debug

--- test/integration/config/initializers/tenanting.rb
+++ test/integration/config/initializers/tenanting.rb
@@ -1,3 +1,5 @@
-Rails.application.configure do |config|
+Rails.application.configure do
   config.middleware.use ActiveRecord::Tenanted::TenantSelector, "ApplicationRecord", ->(request) { request.subdomain }
+
+  config.hosts << /.*\..*\.localhost/ if Rails.env.development?
 end
