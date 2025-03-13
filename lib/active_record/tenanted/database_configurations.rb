20ed42d56d8230f5650f04b023f35c3363289359
Mike Dalessio <mike@37signals.com>
2025-03-13 14:40:56 -0400

    doc: document the SQL query logging hack
    and mark a TODO to upstream it

--- lib/active_record/tenanted/database_configurations.rb
+++ lib/active_record/tenanted/database_configurations.rb
@@ -55,14 +55,20 @@ module ActiveRecord
           configuration_hash.fetch(:tenant)
         end
 
-        def new_connection
-          conn = super
-          conn.class_eval <<~CODE, __FILE__, __LINE__ + 1
-            private def log(sql, name = "SQL", *args, **kwargs, &block)
-              super(sql, "\#{name} [tenant=#{tenant}]", *args, **kwargs, &block)
-            end
-          CODE
-          conn
+        def new_connection # :nodoc:
+          super.tap do |conn|
+            # Let's preserve the tenant name as a string literal in the log method for all tenanted
+            # connections at construction time, so that regardless of whether we're in a proper
+            # tenanted context we are able to log the tenant name when any tenanted connection is
+            # used.
+            #
+            # TODO: this could be upstreamed as a shard-related feature unrelated to tenanting.
+            conn.class_eval <<~CODE, __FILE__, __LINE__ + 1
+              private def log(sql, name = "SQL", *args, **kwargs, &block)
+                super(sql, "\#{name} [tenant=#{tenant}]", *args, **kwargs, &block)
+              end
+            CODE
+          end
         end
 
         def tenanted_config_name
