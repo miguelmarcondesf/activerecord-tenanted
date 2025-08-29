25511c35aa5f72173e7a91bc2f56956f5341bb05
Mike Dalessio <mike@37signals.com>
2025-08-29 16:48:01 -0400

    refactor: clean up global_id

--- lib/active_record/tenanted/global_id.rb
+++ lib/active_record/tenanted/global_id.rb
@@ -11,25 +11,23 @@ module ActiveRecord
 
       class Locator
         def locate(gid, options = {})
-          ensure_tenant_context_safety(gid) if gid.model_class.tenanted?
-
+          ensure_tenant_context_safety(gid)
           gid.model_class.find(gid.model_id)
         end
 
         private
           def ensure_tenant_context_safety(gid)
-            tenant = gid.tenant
-            raise MissingTenantError, "Tenant not present in #{gid.to_s.inspect}" unless tenant
-
             model_class = gid.model_class
-            current_tenant = model_class.current_tenant
-            unless current_tenant.present?
-              raise NoTenantError, "Cannot connect to a tenanted database while untenanted (#{gid})"
-            end
+            return unless model_class.tenanted?
 
-            if tenant != current_tenant
-              raise WrongTenantError,
-                    "GlobalID #{gid.to_s.inspect} does not belong the current tenant #{current_tenant.inspect}"
+            gid_tenant = gid.tenant
+            raise MissingTenantError, "Tenant not present in #{gid.to_s.inspect}" unless gid_tenant
+
+            current_tenant = model_class.current_tenant.presence
+            raise NoTenantError, "Cannot connect to a tenanted database while untenanted (#{gid})" unless current_tenant
+
+            if gid_tenant != current_tenant
+              raise WrongTenantError, "GlobalID #{gid.to_s.inspect} does not belong the current tenant #{current_tenant.inspect}"
             end
           end
       end
