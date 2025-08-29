f676bc0a1f4b01974823d5ad0cf0691906437203
Mike Dalessio <mike@37signals.com>
2025-08-29 16:02:34 -0400

    use `delegate` in subtenant

--- lib/active_record/tenanted/subtenant.rb
+++ lib/active_record/tenanted/subtenant.rb
@@ -10,8 +10,10 @@ module ActiveRecord
           true
         end
 
-        def tenanted_with_class
-          klass = @tenanted_subtenant_of&.constantize || superclass.tenanted_with_class
+        def tenanted_subtenant_of
+          # TODO: cache this / speed this up
+          # but note that we should constantize as late as possible to avoid load order issues
+          klass = @tenanted_subtenant_of&.constantize || superclass.tenanted_subtenant_of
 
           raise Error, "Class #{klass} is not tenanted" unless klass.tenanted?
           raise Error, "Class #{klass} is not a connection class" unless klass.abstract_class?
@@ -19,13 +21,7 @@ module ActiveRecord
           klass
         end
 
-        def current_tenant
-          tenanted_with_class.current_tenant
-        end
-
-        def connection_pool
-          tenanted_with_class.connection_pool
-        end
+        delegate :current_tenant, :connection_pool, to: :tenanted_subtenant_of
       end
 
       prepended do
